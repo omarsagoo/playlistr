58c4c422b311ba8eec49ae1c2b644d8ddb568cc9
omarsagoo <om.sagoo@gmail.com>
2019-10-01 18:41:03 -0700

    add Procfile

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-from bson.objectid import ObjectId
+import os
 
-client = MongoClient()
-db = client.Playlister
-playlists = db.playlists
+from bson.objectid import ObjectId
+from flask import Flask, redirect, render_template, request, url_for
+from pymongo import MongoClient
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
+db = client.get_default_database()
+playlists = db.playlistss
 
 app = Flask(__name__)
 
