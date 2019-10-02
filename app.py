fb53d26f51d2e0fc5304dbefad9e20a5ac7f219d
omarsagoo <om.sagoo@gmail.com>
2019-10-01 22:37:06 -0700

    please

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, render_template, request, url_for
 from pymongo import MongoClient
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
